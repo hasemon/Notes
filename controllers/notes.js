5e3cb9ce3fa297bea696a75998e0ea949bfb5345
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-08-13 00:43:13 +0600

    uibuild

--- controllers/notes.js
+++ controllers/notes.js
@@ -12,32 +12,29 @@ notesRouter.get('/', (request, response) => {
 notesRouter.get('/:id', (request, response, next) => {
     Note.findById(request.params.id)
         .then(note => {
-            if (note){
+            if (note) {
                 response.json(note)
-            }else{
+            } else {
                 response.status(404).end()
             }
         })
+        .catch(error => next(error))
+})
 
-    notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', (request, response, next) => {
     const body = request.body
-
-    if (body.content === undefined) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
 
     const note = new Note({
         content: body.content,
-        important: body.important || false
+        important: body.important || false,
+        date: new Date()
     })
+
     note.save()
         .then(savedNote => {
             response.json(savedNote)
         })
         .catch(error => next(error))
-    })
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
@@ -49,13 +46,15 @@ notesRouter.delete('/:id', (request, response, next) => {
 })
 
 notesRouter.put('/:id',(request, response, next) => {
-    const { content, important } = request.body
+    const body = request.body
 
-    Note.findByIdAndUpdate(request.params.id,
-        { content, important },
-        { new : true , runValidators: true, context: 'query' })
-        .then(
-            updatedNote => {
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new : true })
+        .then(updatedNote => {
                 response.json(updatedNote)
             })
         .catch(error => next(error))
