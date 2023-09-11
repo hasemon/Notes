0de9bce89039817f15d82d7b3b6d4e0356833d86
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-09-12 02:15:51 +0600

    Added the express-async-errors library

--- controllers/notes.js
+++ controllers/notes.js
@@ -8,40 +8,30 @@ notesRouter.get('/',  async (request, response) => {
 
 })
 
-notesRouter.get('/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (note) {
-                response.json(note)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+notesRouter.get('/:id', async (request, response) => {
+
+        const note = await Note.findById(request.params.id)
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
     const body = request.body
 
     const note = new Note({
         content: body.content,
-        important: body.important || false,
-        date: new Date()
+        important: body.important || false
     })
-
-    note.save()
-        .then(savedNote => {
-            response.status(201).json(savedNote)
-        })
-        .catch(error => next(error))
+    const savedNote = await note.save()
+    response.status(201).json(savedNote)
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-        .then(() => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response) => {
+        await Note.findByIdAndRemove(request.params.id)
+        response.status(204).end()
 })
 
 notesRouter.put('/:id',(request, response, next) => {
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -2,25 +2,17 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Note = require('../models/note')
-const {regexpToText} = require("nodemon/lib/utils");
+const helper = require('./test_helper')
 const api = supertest(app)
 
-const initialNotes = [
-    {
-        content: 'HTML is easy',
-        important: false,
-    },
-    {
-        content: 'Browser can execute only JavaScript',
-        important: true,
-    },
-]
 beforeEach(async ()=> {
     await Note.deleteMany({})
-    let noteObject = new Note(initialNotes[0])
-    await noteObject.save()
-    noteObject = new Note(initialNotes[1])
-    await noteObject.save()
+
+    for (let note of helper.initialNotes){
+        let noteObject = new Note(note)
+        await noteObject.save()
+    }
+
 })
 
 
@@ -35,7 +27,7 @@ test('notes are returned as json', async () => {
 test('all notes are retruned', async () => {
     const response = await api.get('/api/notes')
 
-    expect(response.body).toHaveLength(initialNotes.length)
+    expect(response.body).toHaveLength(helper.initialNotes.length)
 })
 
 test('a specific note is within the returned notes', async () => {
@@ -55,14 +47,15 @@ test('a valid note can be added', async () => {
         .send(newNote)
         .expect(201)
         .expect('Content-Type', /application\/json/)
-    const response = await api.get('/api/notes')
-    const contents = response.body.map(r => r.content)
+    const notesAtEnd = await helper.notesInDb()
+    expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1)
 
-    expect(response.body).toHaveLength(initialNotes.length + 1)
+    const contents = notesAtEnd.map(n => n.content)
+
     expect(contents).toContain('async/await simplifies making async calls')
 })
 
-test('note without content is not valid', async () =>{
+test('note without content is not added', async () =>{
     const newNote = {
         important: true
     }
@@ -71,14 +64,36 @@ test('note without content is not valid', async () =>{
         .send(newNote)
         .expect(400)
 
-    const response = await api.get('/api/notes')
-    expect(response.body).toHaveLength(initialNotes.length)
+    const notesAtEnd = await helper.notesInDb()
+    expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
+})
+test('a specific note can be viewed', async ()=> {
+    const notesAtStart = await helper.notesInDb()
+    const noteToView = notesAtStart[0]
+
+    const resultNote = await api
+        .get(`/api/notes/${noteToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    expect(resultNote.body).toEqual(noteToView)
+})
+
+test('a note can be deleted', async ()=> {
+    const noteAtStart = await helper.notesInDb()
+    const noteToDelete = noteAtStart[0]
+
+    await api
+        .delete(`/api/notes/${noteToDelete.id}`)
+        .expect(204)
+
+    const noteAtTheEnd = await helper.notesInDb()
+    expect(noteAtTheEnd).toHaveLength(helper.initialNotes.length -1)
+
+    const contents = noteAtTheEnd.map(r => r.content)
+    expect(contents).not.toContain(noteToDelete.content)
 
 
 })
-
-
-
 
 afterAll(async () => {
     await mongoose.connection.close()
